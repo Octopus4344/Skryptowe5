0a2b5ad2404b3211af60478c1df855d656d378a3
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-04-07 20:24:39 +0200

    Repair after merge

--- zad_2_c.py
+++ zad_2_c.py
@@ -5,7 +5,7 @@ def get_user_from_log(log):
     try:
         description = log["description"]
         pattern = r'user (\S+)(?: \[.*\])?'
-        match =re.search(pattern, description)
+        match = re.search(pattern, description)
         if match:
             return match.group(1)
         else:
