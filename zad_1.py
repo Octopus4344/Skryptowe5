9acee4fd022149d90a60499820027ad9d8fbf80a
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-04-07 17:50:47 +0200

    uzycie wyrazen regularnych i d

--- zad_1.py
+++ zad_1.py
@@ -3,7 +3,8 @@ from typing import List, Dict
 from datetime import datetime
 from zad_2_b import get_ipv4s_from_log
 from zad_2_c import get_user_from_log
-
+from zad_2_d import get_message_type
+import re
 
 def file_reader(file_name):
     # analyzed_lines : List[Dict[]] = []
@@ -31,33 +32,63 @@ def line_analyzer(line) -> dict:
     dictionary['description'] = get_description(line)
     dictionary['IPv4'] = get_ipv4s_from_log(dictionary)
     dictionary['user_name'] = get_user_from_log(dictionary)
+    dictionary['message_type'] = get_message_type(dictionary['description'])
     return dictionary
 
 
 def get_time(line):
-    split_line = line.split()
-    date = " ".join(split_line[:3])
-    # rest = " ".join(split_line[3:])
-    date_time = datetime.strptime(date, "%b %d %H:%M:%S")
-    return date_time
+    # split_line = line.split()
+    # date = " ".join(split_line[:3])
+    # # rest = " ".join(split_line[3:])
+    # date_time = datetime.strptime(date, "%b %d %H:%M:%S")
+    # return date_time
+    pattern = r'^(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})'
+    match = re.search(pattern, line)
+    if match:
+        return datetime.strptime(match.group(1),"%b %d %H:%M:%S")
+    else:
+        return None
 
 
 def get_host_name(line):
-    return line.split()[3].strip()
+    pattern = r'\b\w{3} \d{1,2} \d{2}:\d{2}:\d{2} (\w+)'
+    match = re.search(pattern, line)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
 
 def get_app_component(line):
-    return line.split()[4].split("[")[0].strip()
+    pattern = r'(\w+)\[\d+\]:'
+    match = re.search(pattern,line)
+    if match:
+        return match.group(1)
+    else:
+        return None
+   # return line.split()[4].split("[")[0].strip()
 
 
 def get_pid(line):
-    return line.split()[4].split("[")[1].split("]")[0].strip()
+    pattern = r'\[(\d+)\]:'
+    match = re.search(pattern,line)
+    if match:
+        return match.group(1)
+    else:
+        return None
+    #return line.split()[4].split("[")[1].split("]")[0].strip()
 
 
 def get_description(line):
-    split_line = line.split(":")
-    desc = " ".join(split_line[3:])
-    return desc[1:]
+    pattern = r'\[\d+\]:\s*(.*$)'
+    match = re.search(pattern,line)
+    if match:
+        return match.group(1)
+    else:
+        return None
+    # split_line = line.split(":")
+    # desc = " ".join(split_line[3:])
+    # return desc[1:]
 
 
 if __name__ == "__main__":
--- zad_2_d.py
+++ zad_2_d.py
@@ -1,4 +1,18 @@
 import re
 
+
 def get_message_type(description):
-    return
+    if re.search(r'authentication failure', description, re.IGNORECASE):
+        return "Nieudane logowanie"
+    elif re.search(r'failed password', description, re.IGNORECASE):
+        return "Błędne hasło"
+    elif re.search(r'invalid user', description, re.IGNORECASE):
+        return "Błędna nazwa użytkownika"
+    elif re.search(r'possible break-in attempt', description, re.IGNORECASE):
+        return "Próba włamania"
+    elif re.search(r'connection closed', description, re.IGNORECASE):
+        return "Zamknięcie połączenia"
+    elif re.search(r'accepted', description, re.IGNORECASE):
+        return "Udane logowanie"
+    else:
+        return "inne"
