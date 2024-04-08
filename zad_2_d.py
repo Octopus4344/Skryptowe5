2c7fe81cceffcf714ced6bbc8afbb848b66847c1
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-04-08 07:47:46 +0200

    2d based on enum

--- zad_2_d.py
+++ zad_2_d.py
@@ -1,25 +1,36 @@
 import logging
 import re
+from enum import Enum
+
+
+class LogMessageType(Enum):
+    FAILED_AUTHENTICATION = "Nieudane logowanie"
+    FAILED_PASSWORD = "Błędne hasło"
+    INVALID_USER = "Błędna nazwa użytkownika"
+    BREAK_IN_ATTEMPT = "Próba włamania"
+    CONNECTION_CLOSED = "Zamknięcie połączenia"
+    SUCCESSFUL_LOGIN = "Udane logowanie"
+    OTHER = "inne"
 
 
 def get_message_type(description, logger: logging.Logger):
     if re.search(r'authentication failure', description, re.IGNORECASE):
         logger.warning('Nieudane logowanie w: %s', description)
-        return "Nieudane logowanie"
+        return LogMessageType.FAILED_AUTHENTICATION
     elif re.search(r'failed password', description, re.IGNORECASE):
         logger.error('Błędne hasło w: %s', description)
-        return "Błędne hasło"
+        return LogMessageType.FAILED_PASSWORD
     elif re.search(r'invalid user', description, re.IGNORECASE):
         logger.error('Błędna nazwa użytkownika w: %s', description)
-        return "Błędna nazwa użytkownika"
+        return LogMessageType.INVALID_USER
     elif re.search(r'possible break-in attempt', description, re.IGNORECASE):
         logger.critical("Próba włamania w: %s", description)
-        return "Próba włamania"
+        return LogMessageType.BREAK_IN_ATTEMPT
     elif re.search(r'connection closed', description, re.IGNORECASE):
         logger.info("Zamknięcie połączenia w: %s", description)
-        return "Zamknięcie połączenia"
+        return LogMessageType.CONNECTION_CLOSED
     elif re.search(r'accepted', description, re.IGNORECASE):
         logger.info("Udane logowanie w: %s", description)
-        return "Udane logowanie"
+        return LogMessageType.SUCCESSFUL_LOGIN
     else:
-        return "inne"
+        return LogMessageType.OTHER
