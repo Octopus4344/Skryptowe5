675ed27f003c896bc03bb4f42500767c3af1f753
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-04-10 00:13:09 +0200

    zad5

--- zad_5.py
+++ zad_5.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from datetime import timedelta
 
 from zad_1 import file_reader
 from zad_2_b import get_ipv4s_from_log
@@ -9,6 +10,7 @@ from zad_4 import get_min_max_login
 from zad_4 import get_random_logs_from_user
 from zad_4 import get_stats
 from zad_4 import get_stats_per_user
+from zad_6 import brute_force_detector
 
 
 def main():
@@ -24,23 +26,28 @@ def main():
     subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
     subparsers.add_parser("zad2b", help="Get IPv4 addresses from log file")
-    subparsers.add_parser("zad2c", help="Get user names from log file")
+    subparsers.add_parser("zad2c", help="Get usernames from log file")
     subparsers.add_parser("zad2d", help="Get message types from log file")
 
     zad4a_parser = subparsers.add_parser("zad4a", help="Get random logs from user")
-    zad4a_parser.add_argument("user", help="Username to get logs for")
+    zad4a_parser.add_argument("user", type=str, help="Username to get logs for")
     zad4a_parser.add_argument("count", type=int, help="Number of random logs to retrieve")
 
     subparsers.add_parser("zad4b1", help="Get global statistics")
     subparsers.add_parser("zad4b2", help="Get statistics per user")
     subparsers.add_parser("zad4c", help="Get min and max login users")
 
+    zad6_parser = subparsers.add_parser("zad6", help="Brute force detector")
+    zad6_parser.add_argument("interval", help="Time interval in hours for brute force detection",
+                             default=1, type=int)
+    zad6_parser.add_argument("--user", help="Check only for logs from a single user", action="store_true")
+    # zad6_parser.add_argument("--multi_user", dest="user", action="store_false")
+    zad6_parser.set_defaults(user=False)
+
     args = parser.parse_args()
 
     # Ustawienie poziomu logowania
-    if args.loglevel == "none":
-        logging.disable(logging.CRITICAL + 1)
-    elif args.loglevel == "critical":
+    if args.loglevel == "critical":
         logging.basicConfig(level=logging.CRITICAL)
     elif args.loglevel == "error":
         logging.basicConfig(level=logging.ERROR)
@@ -50,6 +57,8 @@ def main():
         logging.basicConfig(level=logging.INFO)
     elif args.loglevel == "debug":
         logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.disable(logging.CRITICAL + 1)
 
     # Przekazanie argumentów i podkomend do odpowiednich funkcji
     logs = file_reader(args.logfile)
@@ -76,6 +85,11 @@ def main():
         min_login, max_login = get_min_max_login(logs)
         print(f"Min log-ins: {min_login}")
         print(f"Max log-ins: {max_login}")
+    elif args.subcommand == "zad6":
+        for line in brute_force_detector(logs, timedelta(hours=args.interval), True):
+            print()
+            for key, value in line.items():
+                print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
